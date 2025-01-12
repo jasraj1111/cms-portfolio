f777ad894d0189919653bd3bf416ce09087d3997
Jasraj Shendge <jasraj.229301558@muj.manipal.edu>
2025-01-12 14:47:10 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,10 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.log(err));
 
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server running on port ${process.env.PORT || 5000}`);
+      });
+
 // Routes
 app.use('/api/projects', require('./routes/projectRoutes'));
 app.use('/api/auth', require('./routes/authRoutes'));
